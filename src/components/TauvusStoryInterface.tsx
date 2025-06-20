dc61ad4f56c179b7e56d0c7066a5cfc379bd0b8b
Aditya Kumar <44408488+xVergilSparda@users.noreply.github.com>
2025-06-20 03:25:16 -0700

    Fix SVG URL syntax error in TauvusStoryInterface

--- src/components/TauvusStoryInterface.tsx
+++ src/components/TauvusStoryInterface.tsx
@@ -274,7 +274,7 @@ const TauvusStoryInterface: React.FC<TauvusStoryInterfaceProps> = ({
   return (
     <div className="min-h-screen bg-gradient-to-br from-purple-900 via-blue-900 to-indigo-900 relative overflow-hidden">
       {/* Background Effects */}
-      <div className="absolute inset-0 bg-[url('data:image/svg+xml,%3Csvg width="60" height="60" viewBox="0 0 60 60" xmlns="http://www.w3.org/2000/svg"%3E%3Cg fill="none" fill-rule="evenodd"%3E%3Cg fill="%23ffffff" fill-opacity="0.1"%3E%3Ccircle cx="30" cy="30" r="2"/%3E%3C/g%3E%3C/g%3E%3C/svg%3E')] opacity-30"></div>
+      <div className="absolute inset-0 bg-[url(\"data:image/svg+xml,%3Csvg width='60' height='60' viewBox='0 0 60 60' xmlns='http://www.w3.org/2000/svg'%3E%3Cg fill='none' fill-rule='evenodd'%3E%3Cg fill='%23ffffff' fill-opacity='0.1'%3E%3Ccircle cx='30' cy='30' r='2'/%3E%3C/g%3E%3C/g%3E%3C/svg%3E\")] opacity-30"></div>
       
       {/* Top Controls */}
       <div className="absolute top-4 left-4 right-4 z-20">
